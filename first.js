44eed02263ef66cc36bf87a59ba9f18370470d10
Francisco Kahil <codexico@gmail.com>
2021-05-04 12:54:12 +0300

    f

--- first.js
+++ first.js
@@ -3,7 +3,7 @@ var http = require('http');
 var axios = require('axios')
 const puppeteer = require('puppeteer');
 http.createServer(async function (req, res) {
-    const profileId = '9b3af9d4-862b-4e28-a29a-56afe85a23e5';
+    const profileId = 'f662c476-7c22-43fd-9118-bb3aca8f26e2';
     const mlaPort = 35000;
     const stringReq = `http://127.0.0.1:${mlaPort}/api/v1/profile/start?automation=true&puppeteer=true&profileId=${profileId}`
     const resp = await axios.get(stringReq)
